89e7e460c4a6e4f09cebfbc87ed50c904a22c12e
saymon <saymon.anjos@eng4tech.com.br>
2024-01-08 15:50:11 -0300

    Add azure data factory bicep template

---
+++ ps-scripts/azure-deployment.ps1
@@ -0,0 +1,16 @@
+$CustomerName = 'neolude'
+$Environment = 'dev'
+$Department = 'bi'
+$Location = 'brazilsouth'
+$resourceGroupName = "$($CustomerName)-$($Department)-$($Environment)-$($Location)"
+
+New-AzSubscriptionDeployment `
+    -resourceGroupName $resourceGroupName `
+    -Location $Location `
+    -TemplateFile '.\bicep\resourceGroup.bicep' `
+    -resourceGroupLocation $Location
+
+New-AzResourceGroupDeployment `
+    -resourceGroupName $resourceGroupName `
+    -TemplateFile '.\bicep\azureDataFactory.bicep' `
+    -Mode Incremental
