938feff8941bc255d1de2796c86c703baa349cda
saymon <saymon.anjos@eng4tech.com.br>
2024-01-08 16:50:52 -0300

    Add azure data factory linked services template

---
+++ scripts/azure-deployment.ps1
@@ -0,0 +1,29 @@
+$CustomerName = 'neolude'
+$Environment = 'dev'
+$Department = 'bi'
+$Location = 'brazilsouth'
+$resourceGroupName = "$($CustomerName)-$($Department)-$($Environment)-$($Location)"
+$baseURL = 'https://data-api.neolude.com.br/'
+$relativeURL = '/'
+
+New-AzSubscriptionDeployment `
+    -resourceGroupName $resourceGroupName `
+    -Location $Location `
+    -TemplateFile '..\bicep\resourceGroup.bicep' `
+    -resourceGroupLocation $Location
+
+New-AzResourceGroupDeployment `
+    -resourceGroupName $resourceGroupName `
+    -TemplateFile '..\bicep\azureDataFactory.bicep' `
+    -Mode Incremental
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile '..\bicep\sqlServer.bicep' `
+    -Mode Incremental
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile '..\bicep\azureDataFactoryLinkedServices.bicep' `
+    -baseURL $baseURL `
+    -Mode Incremental
