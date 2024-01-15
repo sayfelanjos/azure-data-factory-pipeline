9529023c42135e903f5c5467a3e9a61474802198
saymon <saymon.anjos@eng4tech.com.br>
2024-01-15 14:00:34 -0300

    Fix sql database tables names

--- ps-scripts/sql-server-deployment.ps1
+++ ps-scripts/sql-server-deployment.ps1
@@ -4,7 +4,7 @@ $dbName = $args[2]
 $adminLogin = $args[3]
 # $adminPassword = ConvertTo-SecureString $args[4] -AsPlainText -Force
 $adminPassword = $args[4]
-$templatePath = 'C:\Users\saymo\eng4tech\neolude\LAB-NEOLUDE-DATAOPS\bicep\database\sqlServer.bicep'
+$templatePath = '.\bicep\database\sqlServer.bicep'
 
 $isPresent = Get-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $serverName -ErrorAction SilentlyContinue
 if ($isPresent) {
