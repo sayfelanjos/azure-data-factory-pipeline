42a157c81e8c48e0c8c656a72aa44c6d27a407a2
saymon.anjos <saymon.anjos@eng4tech.com.br>
2024-01-15 17:37:09 +0000

    Updated sql-server-deployment.ps1

--- ps-scripts/sql-server-deployment.ps1
+++ ps-scripts/sql-server-deployment.ps1
@@ -2,7 +2,6 @@ $resourceGroupName = $args[0]
 $serverName = $args[1]
 $dbName = $args[2]
 $adminLogin = $args[3]
-# $adminPassword = ConvertTo-SecureString $args[4] -AsPlainText -Force
 $adminPassword = $args[4]
 $templatePath = '.\bicep\database\sqlServer.bicep'
 
@@ -10,11 +9,5 @@ $isPresent = Get-AzSqlServer -ResourceGroupName $resourceGroupName -ServerName $
 if ($isPresent) {
     Write-Host 'Server already exists!'
 }else {
-    New-AzResourceGroupDeployment `
-        -ResourceGroupName $resourceGroupName `
-        -TemplateFile $templatePath `
-        -serverName $serverName `
-        -dbName $dbName `
-        -sqlServerAdminLogin $adminLogin `
-        -sqlServerAdminPassword $adminPassword
+    New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templatePath -serverName $serverName -dbName $dbName -sqlServerAdminLogin $adminLogin -sqlServerAdminPassword $adminPassword
 }
