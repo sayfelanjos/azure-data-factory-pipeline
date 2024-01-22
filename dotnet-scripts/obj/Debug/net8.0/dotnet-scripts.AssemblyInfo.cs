6011e7f8e636d805ae1a3cc20252c2926107974f
saymon <saymon.anjos@eng4tech.com.br>
2024-01-22 12:43:58 -0300

    Add postman collection file

--- dotnet-scripts/obj/Debug/net8.0/dotnet-scripts.AssemblyInfo.cs
+++ dotnet-scripts/obj/Debug/net8.0/dotnet-scripts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-scripts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941ba6dc71dd232235e5e504d8c9a87e2f07c039")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2441a85290af2159c258aeaf7b668581c8c439a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-scripts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-scripts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
