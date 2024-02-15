82afde4f5a89fb826adfe56848565ef300156326
saymon <saymon.anjos@eng4tech.com.br>
2024-02-15 20:41:43 -0300

    Add new pipelines execution flow

--- ps-scripts/pipelines-deployment.ps1
+++ ps-scripts/pipelines-deployment.ps1
@@ -1,9 +1,9 @@
 $pipelinesPath = '.\bicep\pipelines'
 $copyPipelinesPath = '.\bicep\copyPipelines'
+$pipelines = Get-ChildItem $pipelinesPath
 $copyPipelines = Get-ChildItem $copyPipelinesPath
 $resourceGroupName = $args[0]
 $dataFactoryName = $args[1]
-$updateStartDate = $args[2]
 
 # New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$($pipelinesPath)\contents_pl.bicep" -pipelineName 'contents_pl' -dataFactoryName $dataFactoryName
 # New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$($pipelinesPath)\contents_pl_2.bicep" -pipelineName 'contents_pl_2' -dataFactoryName $dataFactoryName
@@ -22,11 +22,10 @@ foreach ($pl in $copyPipelines) {
         -ErrorAction SilentlyContinue
 }
 
-foreach ($pl in $pipelinesPath) {
+foreach ($pl in $pipelines) {
     New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
     -TemplateFile $pl.FullName `
     -dataFactoryName $dataFactoryName `
-    -updateStartDate $updateStartDate `
     -ErrorAction SilentlyContinue
 }
 
